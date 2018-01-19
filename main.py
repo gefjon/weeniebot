f2c71d60ecd2b0602a9ce4cd7b895f95d8d383c4
Arthur Goldman <arthur@goldman-tribe.org>
2018-01-19 16:58:10 +0100

    it's aliiiiive

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ def main():
     auth = tweepy.OAuthHandler(secret.CONSUMER_TOKEN, secret.CONSUMER_SECRET)
     auth.set_access_token(secret.ACCESS_TOKEN, secret.ACCESS_SECRET)
     api = tweepy.API(auth)
-    listener = WeenieListener()
-    stream = tweepy.Stream(auth=api.auth, listener=listener())
-    stream.filter(track=['@_weenie_bot'])
+    listener = WeenieListener(api)
+    stream = tweepy.Stream(auth=api.auth, listener=listener)
+    try:
+        stream.filter(track=['@_weenie_bot'])
+    except KeyboardInterrupt:
+        quit()
 
 
 if __name__ == "__main__":
---
+++ responder.py
@@ -0,0 +1,46 @@
+import threading
+import secret
+import tweepy
+import re
+
+
+class RespondingThread(threading.Thread):
+    def __init__(self, tweet):
+        self.tweet = tweet
+        threading.Thread.__init__(self)
+
+    def tweet_reply(self, text):
+        auth = tweepy.OAuthHandler(secret.CONSUMER_TOKEN, secret.CONSUMER_SECRET)
+        auth.set_access_token(secret.ACCESS_TOKEN, secret.ACCESS_SECRET)
+        api = tweepy.API(auth)
+        api.update_status(text, self.tweet.id)
+
+    def single_person(self, weenie):
+        if weenie == '@{}'.format(self.tweet.user.screen_name):
+            return '{} you are a weenie'.format(weenie)
+        else:
+            return '{} is are a weenie'.format(weenie)
+
+    def no_names(self):
+        return '@{} you are a weenie'.format(self.tweet.user.screen_name)
+
+    def many_people(self, weenies):
+        response = ''
+        for weenie in weenies:
+            response += '{} '.format(weenie)
+        return response + ' are all a bunch of weenies'
+
+    def run(self):
+        regex = re.compile('(?<=^|(?<=[^a-zA-Z0-9-_\.]))(@[A-Za-z0-9_]+)')
+
+        to_weenie = []
+        usernames = regex.findall(self.tweet.text)
+        for user in usernames:
+            if user != '@_weenie_bot':
+                to_weenie.append(user)
+        if len(to_weenie) == 0:
+            self.tweet_reply(self.no_names())
+        elif len(to_weenie) == 1:
+            self.tweet_reply(self.single_person(to_weenie[0]))
+        else:
+            self.tweet_reply(self.many_people(to_weenie))
